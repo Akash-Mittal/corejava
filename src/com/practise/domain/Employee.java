82604f80dbe397785bd7af11973625cb556bb179
Akash-Mittal <mittal.akash.adam@gmail.com>
2018-10-08 14:55:51 +0530

    Changes:1.) Added One More Example.

--- src/com/practise/domain/Employee.java
+++ src/com/practise/domain/Employee.java
@@ -1,18 +1,36 @@
 package com.practise.domain;
 
 import java.io.Serializable;
-import java.sql.ResultSet;
-import java.util.*;
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.Comparator;
+import java.util.HashSet;
+import java.util.List;
+import java.util.Random;
+import java.util.Set;
+import java.util.TreeSet;
 
+public class Employee implements Comparable<Employee>, Serializable {
 
+	public String getEmployeeName() {
+		return employeeName;
+	}
 
-public class Employee implements Comparable<Employee> , Serializable {
-	
+	public void setEmployeeName(String employeeName) {
+		this.employeeName = employeeName;
+	}
+
+	public int getEmployeeID() {
+		return employeeID;
+	}
+
+	public void setEmployeeID(int employeeID) {
+		this.employeeID = employeeID;
+	}
+
 	private static final long serialVersionUID = 1L;
-	//private static final long serialVersionUID = 6884062097665486547L;
-	
-	
-	
+	// private static final long serialVersionUID = 6884062097665486547L;
+
 	private String employeeName;
 	private int employeeID;
 
@@ -20,7 +38,7 @@ public class Employee implements Comparable<Employee> , Serializable {
 	public int compareTo(Employee o) {
 		return this.employeeName.compareTo(o.employeeName);
 	}
-	
+
 	public Employee(String employeeName, int employeeID) {
 		super();
 		this.employeeName = employeeName;
@@ -29,13 +47,13 @@ public class Employee implements Comparable<Employee> , Serializable {
 
 	@Override
 	public String toString() {
-		return "Employee [employeeName=" + employeeName + ", employeeID="
-				+ employeeID + "]";
+		return "Employee [employeeName=" + employeeName + ", employeeID=" + employeeID + "]";
 	}
+
 	public static void main(String[] args) {
-		
+
 //*************************LIST************************************		
-		
+
 		// Sorting on the Basis of employeeName
 		List<Employee> list = new ArrayList<Employee>();
 		list.add(new Employee("b", 1));
@@ -43,7 +61,7 @@ public class Employee implements Comparable<Employee> , Serializable {
 		list.add(new Employee("a", 2));
 		list.add(new Employee("z", 2));
 		System.err.println("----------Unordered List-----------");
-		for(Employee employee : list)
+		for (Employee employee : list)
 			System.out.println(employee);
 		Comparator<Employee> comparator = new Comparator<Employee>() {
 			@Override
@@ -51,36 +69,35 @@ public class Employee implements Comparable<Employee> , Serializable {
 				return o1.employeeName.compareTo(o2.employeeName);
 			}
 		};
-		
-		Collections.sort(list,comparator);
+
+		Collections.sort(list, comparator);
 		System.err.println("----------Ordered List-----------");
-		for(Employee employee : list)
-			System.out.println(employee);
-		
-//*************************LIST************************************
-		//Sortin a  Set of user defined POJO
-			// 	Tree Set Using Comparable
-			// List Using Comparator
-		Set<Employee> set = new HashSet<Employee>();
-		for(int i=0;i<5;i++){
-			set.add(new Employee("a"+new Random().nextInt(10), i));
-		}
-		System.err.println("----------Unordered Set-----------");
-		for(Employee employee : set)
-			System.out.println(employee);
-		System.err.println("----------Ordered Tree Set-----------");	
-		// The Employee must implement Comparable interface
-			// Else it will throw ClassCastException
-		Set<Employee> set1 = new TreeSet<>(set);
-		for(Employee employee : set1)
-		System.out.println(employee);
-		
-		List<Employee> list2 = new ArrayList<Employee>(set);
-		System.err.println("----------Ordered  Set through List-----------");
-		Collections.sort(list2,comparator);
-		for(Employee employee : list2)
+		for (Employee employee : list)
 			System.out.println(employee);
 
-		
+//*************************LIST************************************
+		// Sortin a Set of user defined POJO
+		// Tree Set Using Comparable
+		// List Using Comparator
+		Set<Employee> set = new HashSet<Employee>();
+		for (int i = 0; i < 5; i++) {
+			set.add(new Employee("a" + new Random().nextInt(10), i));
+		}
+		System.err.println("----------Unordered Set-----------");
+		for (Employee employee : set)
+			System.out.println(employee);
+		System.err.println("----------Ordered Tree Set-----------");
+		// The Employee must implement Comparable interface
+		// Else it will throw ClassCastException
+		Set<Employee> set1 = new TreeSet<>(set);
+		for (Employee employee : set1)
+			System.out.println(employee);
+
+		List<Employee> list2 = new ArrayList<Employee>(set);
+		System.err.println("----------Ordered  Set through List-----------");
+		Collections.sort(list2, comparator);
+		for (Employee employee : list2)
+			System.out.println(employee);
+
 	}
 }
