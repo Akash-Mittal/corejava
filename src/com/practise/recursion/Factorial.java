1f682be5a52e603caccf6d5e63bab36554ae2b3d
Akash-Mittal <mittal.akash.adam@gmail.com>
2018-10-10 10:15:31 +0530

    Changes:1.) Factorial

--- src/com/practise/recursion/Factorial.java
+++ src/com/practise/recursion/Factorial.java
@@ -12,29 +12,62 @@ public class Factorial {
 
 	Predicate<Number> checkIfNotNull = input -> input != null;
 	Predicate<Number> checkIfValueGreaterThan0 = input -> input.intValue() > 0;
+	Predicate<Number> checkIfValueEqualTo1 = input -> input.intValue() == 1;
+	Predicate<Number> checkIfValueLessThan2 = input -> input.intValue() < 2;
 
-	public String getFactorial(Integer number) {
+	public String getBigFactorialRecursive(Integer number) {
 		BigDecimal factorial = BigDecimal.ZERO;
-		if (checkIfNotNull.and(checkIfValueGreaterThan0).test(number)) {
-			factorial = BigDecimal.ONE;
-			for (int i = number; i > 0; i--) {
-				factorial = factorial.multiply(BigDecimal.valueOf(i));
+		if (checkIfNotNull.test(number)) {
+			if ((checkIfValueGreaterThan0).test(number)) {
+				factorial = BigDecimal.ONE;
+				for (int i = number; i > 0; i--) {
+					factorial = factorial.multiply(BigDecimal.valueOf(i));
+				}
+			} else {
+				return "-1";
 			}
+		} else {
+			return "-1";
 		}
 		System.out.println(factorial.toPlainString());
 		return factorial.toPlainString();
 
 	}
 
+	public int getFactorialRecursiveSimple(int number) {
+		if (checkIfValueGreaterThan0.test(number)) {
+			if (checkIfValueEqualTo1.test(number)) {
+				return 1;
+			} else {
+				return number * getFactorialRecursiveSimple(number - 1);
+			}
+		} else {
+			return -1;
+		}
+
+	}
+
+	public String getBigFactorialRecursive(BigDecimal factorial) {
+		if (checkIfValueGreaterThan0.test(factorial)) {
+			if ((checkIfValueEqualTo1).test(factorial)) {
+				return "1";
+			} else {
+				return factorial.multiply(new BigDecimal(getBigFactorialRecursive(factorial.subtract(BigDecimal.ONE))))
+						.toPlainString();
+			}
+		} else {
+			return "-1";
+		}
+
+	}
+
 	@Test
 	public void testFactorialValid() {
 		Factorial factorial = new Factorial();
-		Assert.assertThat(factorial.getFactorial(null), is(BigDecimal.ZERO.toPlainString()));
-		Assert.assertThat(factorial.getFactorial(0), is(BigDecimal.ZERO.toPlainString()));
-		Assert.assertThat(factorial.getFactorial(1), is(BigDecimal.ONE.toPlainString()));
-		Assert.assertThat(factorial.getFactorial(2), is(BigDecimal.valueOf(2).toPlainString()));
-		Assert.assertThat(factorial.getFactorial(3), is(BigDecimal.valueOf(6).toPlainString()));
-		Assert.assertThat(factorial.getFactorial(100), is(
-				"93326215443944152681699238856266700490715968264381621468592963895217599993229915608941463976156518286253697920827223758251185210916864000000000000000000000000"));
+		for (int i = -10; i < 14; i++) {
+			Assert.assertThat(factorial.getBigFactorialRecursive(i), is(factorial.getFactorialRecursiveSimple(i) + ""));
+			Assert.assertThat(factorial.getBigFactorialRecursive(i),
+					is(factorial.getBigFactorialRecursive(BigDecimal.valueOf(i))));
+		}
 	}
 }
---
+++ src/com/practise/recursion/README.md
@@ -0,0 +1,37 @@
+# Factorial
+
+A Simple yet Effective Way to learn Recursion.
+
+## This Artifact should be able to answer these questions.
+
+* What are the Advantages and Disadvantages of Using recursion ?
+* How to Get the Factorial of a Very Large Number ?
+* How to fasten Test Driven Development ?
+
+---
+## The Following Program Consists of:
+
+1.) Simple `Recursive` Implementation With Primitive Type of `int`. 
+2.) `Iterative` Implementation to support `big-data` using `BigDecimal`.
+3.) `Recursive` Implementation to support `big-data` using `BigDecimal`.
+
+## Learnings:
+
+* `Recursion` is a Program Calling itself to do same task.
+* The said task should be identical in nature.
+* The Current Task completion is dependent on Previous task.
+* The BigDecimal Was Used to very Large Numbers.
+* Primitive `int` will be able to calculate till 12! only. 
+
+### Disadvantage
+
+* Cannot do validations, or simply put the Recursive function will call itself
+  , when it does any Validations like `if not null` will be executed.
+  
+* Keeping that as an assumption it is safe to say that `Recursive Functions` are
+
+	  * Dependent or on the Mercy of Calling Function for Validation Checks.
+	  * If you are brave enough to put `not null`, good  luck with your coding career :-.
+  
+
+  
