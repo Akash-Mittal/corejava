3172bc490009323545c8942c734b3ce3b724d08a
Akash-Mittal <mittal.akash.adam@gmail.com>
2018-10-31 16:24:34 +0530

    Changes:1.) Practise.

--- src/test/java/com/practise/java8/OptionalTest.java
+++ src/test/java/com/practise/java8/OptionalTest.java
@@ -23,6 +23,15 @@ public class OptionalTest {
 		assertNotNull(Optional.ofNullable(value));
 	}
 
+	@Test(expected = InvalidArgumentException.class)
+	public void testWhenConvertingAnObjectToOptionalUsingOffNullableThrowException() {
+		String value = null;
+		Optional<String> val = Optional.ofNullable(value);
+		String retrievedVal = val.orElseThrow(() -> {
+			throw new InvalidArgumentException("Invalid");
+		});
+	}
+
 	@Test
 	public void testWhenGettingValueFromOptionalAndValueIsNull() {
 		String value = null;
@@ -48,6 +57,41 @@ public class OptionalTest {
 			finalResponse.append(defaultValue);
 		});
 		assertThat(finalResponse.toString(), is("Succesfull:" + value));
+	}
+
+}
+
+class InvalidArgumentException extends RuntimeException {
+
+	/**
+	 * 
+	 */
+	private static final long serialVersionUID = -7243730349806241412L;
+
+	public InvalidArgumentException() {
+		super();
+		// TODO Auto-generated constructor stub
+	}
+
+	public InvalidArgumentException(String message, Throwable cause, boolean enableSuppression,
+			boolean writableStackTrace) {
+		super(message, cause, enableSuppression, writableStackTrace);
+		// TODO Auto-generated constructor stub
+	}
+
+	public InvalidArgumentException(String message, Throwable cause) {
+		super(message, cause);
+		// TODO Auto-generated constructor stub
+	}
+
+	public InvalidArgumentException(String message) {
+		super(message);
+		// TODO Auto-generated constructor stub
+	}
+
+	public InvalidArgumentException(Throwable cause) {
+		super(cause);
+		// TODO Auto-generated constructor stub
 	}
 
 }
